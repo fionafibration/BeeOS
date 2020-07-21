d8317590b002e3eaa48464d63c229a78d45f9f4e
Finian Blackett <spamsuckersunited@gmail.com>
2020-07-21 17:37:30 -0600

    remove WSL compat

--- run-bee-os.sh
+++ run-bee-os.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-if [ $1 ] 
-then
-	# Compat for WSL or other remote XWindow systems
-	export DISPLAY=:0
-fi
-
 nasm -fbin boot_sector.asm -o beeOS.bin
 qemu-system-x86_64 -drive format=raw,file=beeOS.bin
 
